e2db6bc416a9071fbb43132022a7c28d7e7a97c4
idea_liujl <idea_liujl@idea-liujldeMac.local>
2017-07-14 15:58:23 +0800

    ui_1

---
+++ ui_1/Vacation.swift
@@ -0,0 +1,9 @@
+//
+//  Vacation.swift
+//  ui_1
+//
+//  Created by idea_liujl on 17/7/14.
+//  Copyright © 2017年 idea_liujl. All rights reserved.
+//
+
+import Foundation
---
+++ ui_1/addVViewController.swift
@@ -0,0 +1,35 @@
+//
+//  addVViewController.swift
+//  ui_1
+//
+//  Created by idea_liujl on 17/7/14.
+//  Copyright © 2017年 idea_liujl. All rights reserved.
+//
+
+import UIKit
+
+class addVViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
